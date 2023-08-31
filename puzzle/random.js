495ce468ebf76187802df0cf97953f7ebba0449e
speechincoherent <theswirca@gmail.com>
2023-08-31 15:17:45 -0500

    kinda dumb

--- puzzle/random.js
+++ puzzle/random.js
@@ -1,16 +1,16 @@
-var x = Math.floor(Math.random*5)+1
+var x = Math.floor(Math.random*5)+1;
 if (x = 1) {
-    Document.getElementById("titleRandom").src= "../assets/puzzle/title-1.png"
+    Document.getElementById("titleRandom").src= "../assets/puzzle/title-1.png";
 }
 else if (x = 2) {
-    Document.getElementById("titleRandom").src= "../assets/puzzle/title-2.png"
+    Document.getElementById("titleRandom").src= "../assets/puzzle/title-2.png";
 } 
 else if (x = 3) {
-    Document.getElementById("titleRandom").src= "../assets/puzzle/titulo-3.png"
+    Document.getElementById("titleRandom").src= "../assets/puzzle/titulo-3.png";
 } 
 else if (x = 4) {
-    Document.getElementById("titleRandom").src= "../assets/puzzle/title-4.png"
+    Document.getElementById("titleRandom").src= "../assets/puzzle/title-4.png";
 } 
 else if (x = 5) {
-    Document.getElementById("titleRandom").src= "../assets/puzzle/title-5.png"
+    Document.getElementById("titleRandom").src= "../assets/puzzle/title-5.png";
 } 
