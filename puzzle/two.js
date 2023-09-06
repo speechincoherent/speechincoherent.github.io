ce4f7b683975c3bd46612bb9eb87d3dd8b9d85e9
speechincoherent <theswirca@gmail.com>
2023-09-06 17:31:21 -0500

    d

--- puzzle/two.js
+++ puzzle/two.js
@@ -5,7 +5,6 @@ function numberChecker() {
     var a = document.getElementById("input").value;
     if (number = 3 || number == "three") {
         window.location.href = "https://speechincoherent.github.io/puzzle/eerht.html";
-        window.localStorage.setItem('level3', "done")
     }
     else {
         document.getElementById("output").innerHTML = "try again";
