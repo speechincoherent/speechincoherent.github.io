f8b3874141e97cec53cd773e6bccec5b4b93371b
speechincoherent <theswirca@gmail.com>
2023-09-08 18:07:31 -0500

    button group

---
+++ redirects.js
@@ -0,0 +1,7 @@
+function redirectLinks() {
+    window.location.href = "https://speechincoherent.github.io/links"
+}
+function redirectContact() {
+    window.location.href = "https://speechincoherent.github.io/contact"
+}
+    
