55a0ea408628d54b9689a7f75bd5bfca3a6b112b
speechincoherent <theswirca@gmail.com>
2024-04-10 10:01:43 -0500

    Update levelmilestones.js

--- puzzle/levelmilestones.js
+++ puzzle/levelmilestones.js
@@ -1,18 +1,21 @@
 const milestones = ["hydrogen"];
-window.onload = if (milestones.includes(this.href.substring(this.href.lastIndexOf('/') + 1))) {
+window.onload = logMilestone()
+function logmilestone() {
+if (milestones.includes(this.href.substring(this.href.lastIndexOf('/') + 1))) {
     var levelMilestone = getCookie("levelMilestone")
     if (levelMilestone == null) {
-        document.cookie = "levelMilestone"=this.href.substring(this.href.lastIndexOf('/') + 1); path=/puzzle
+        document.cookie = "levelMilestone"=this.href.substring(this.href.lastIndexOf('/') + 1); path=/puzzle/;
     }
     else {
-        let currentLevelPos = milestones.indexOf(this.href.substring(this.href.lastIndexOf('/') + 1))
-        let levelMilestonePos = indexOf(levelMilestone)
+        let currentLevelPos = milestones.indexOf(this.href.substring(this.href.lastIndexOf('/') + 1));
+        let levelMilestonePos = indexOf(levelMilestone);
             if (currentLevelPos > levelMilestonePos) {
-                document.cookie = "levelMilestone"=this.href.substring(this.href.lastIndexOf('/') + 1); path=/puzzle
+                document.cookie = "levelMilestone"=this.href.substring(this.href.lastIndexOf('/') + 1); path=/puzzle/;
             }
             else {
-
+                void(0)
             }
-    }   
-}
+        };
+    };
+};
 
