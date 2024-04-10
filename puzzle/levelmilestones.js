051642a146243f2b0d8a1ed618f914daa9d13ab6
speechincoherent <theswirca@gmail.com>
2024-04-10 09:47:19 -0500

    level 10 added p, created levelmilestones.js, fixed maranga.html

---
+++ puzzle/levelmilestones.js
@@ -0,0 +1,18 @@
+const milestones = ["hydrogen"];
+window.onload = if (milestones.includes(this.href.substring(this.href.lastIndexOf('/') + 1))) {
+    var levelMilestone = getCookie("levelMilestone")
+    if (levelMilestone == null) {
+        document.cookie = "levelMilestone"=this.href.substring(this.href.lastIndexOf('/') + 1); path=/puzzle
+    }
+    else {
+        let currentLevelPos = milestones.indexOf(this.href.substring(this.href.lastIndexOf('/') + 1))
+        let levelMilestonePos = indexOf(levelMilestone)
+            if (currentLevelPos > levelMilestonePos) {
+                document.cookie = "levelMilestone"=this.href.substring(this.href.lastIndexOf('/') + 1); path=/puzzle
+            }
+            else {
+
+            }
+    }   
+}
+
