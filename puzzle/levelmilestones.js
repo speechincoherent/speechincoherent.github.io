15d5738c2a6e11fcf1e0e649858ac68f732ab1ca
speechincoherent <theswirca@gmail.com>
2024-04-10 20:14:37 -0500

    Update levelmilestones.js

--- puzzle/levelmilestones.js
+++ puzzle/levelmilestones.js
@@ -1,5 +1,16 @@
+if (getCookie('hydrogen')) {
+document.cookie = "hydrogen=false";
+}
+let currentLevel = this.href.substring(this.href.lastIndexOf('/') + 1);
+if (currentLevel == "hydrogen") {
+    document.cookie = "hydrogen=true"
+};
+
+
+
+
+// scrap code, i don't wanna just delete it though so it stays here
 const milestones = ["hydrogen"];
-window.onload = logMilestone()
 function logmilestone() {
 if (milestones.includes(this.href.substring(this.href.lastIndexOf('/') + 1))) {
     var levelMilestone = getCookie("levelMilestone")
